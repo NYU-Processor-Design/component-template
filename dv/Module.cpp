dd6fabeae23d8ecce0e6e787aec736e1d55420d4
Vito Gamberini <vito@gamberini.email>
2024-02-07 04:42:24 -0500

    refactor: Update to current usages
    * Update .clang-format
    * Update vcpkg baselines
    * Inline vcpkg-configuration
    * Remove dependency lines from coverage data
    * Use testutil correctly in dv/Module.cpp
    * Add NYU_GEN_TRACES for enabling transparent tracers

--- dv/Module.cpp
+++ dv/Module.cpp
@@ -1,8 +1,9 @@
 #include <catch2/catch_test_macros.hpp>
+#include <NyuTestUtil.hpp>
+
 #include <VModule.h>
 
-
 TEST_CASE("Test") {
-  VModule model;
+  auto& mod {nyu::getDUT<VModule>()};
   REQUIRE(1);
 }
