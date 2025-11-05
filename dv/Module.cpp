5601fe78320842e6871522d259be3adcce6c41c0
Vito Gamberini <vito@gamberini.email>
2025-11-05 15:58:16 -0500

    refactor: update to modern usage
    * use vcpkg cahce
    * BootStrap -> Bootstrap
    * NyuTestUtils v2

--- dv/Module.cpp
+++ dv/Module.cpp
@@ -1,9 +1,9 @@
 #include <catch2/catch_test_macros.hpp>
-#include <NyuTestUtil.hpp>
+#include <NyuCatch2TestUtil.hpp>
 
 #include <VModule.h>
 
 TEST_CASE("Test") {
-  auto& mod {nyu::getDUT<VModule>()};
+  auto& mod {nyu::get_dut_catch2<VModule>()};
   REQUIRE(1);
 }
